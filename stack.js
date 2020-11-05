54e690fb1d3af31c3c482aff59f122d16027a27f
sun-weitao <1457928205@qq.com>
2020-11-05 20:52:44 +0800

    update

--- stack.js
+++ stack.js
@@ -24,3 +24,41 @@ function Stack(){
 }
 
 // [111,'+',222+'*'+123]
+
+
+function evalRPN(input){
+    let stack = new Stack();
+    let op1,op2;
+    for(let item of input){
+        switch(item){
+            case '+':
+                op2 = stack.pop();
+                op1 = stack.pop();
+                stack.push(op1 + op2);
+                break;
+            case '-':
+                op2 = stack.pop();
+                op1 = stack.pop();
+                stack.push(op1 - op2);
+                break;
+            case '*':
+                op2 = stack.pop();
+                op1 = stack.pop();
+                stack.push(op1 * op2);
+                break;
+            case '/':
+                op2 = stack.pop();
+                op1 = stack.pop();
+                stack.push(op1 / op2);
+                break;
+            default:
+                stack.push(Number(item));
+                break;
+        }
+    }
+    return stack.pop();
+}
+
+const arr = ['2','1','+','3','*'];
+const arr2 = ["4", "13", "5", "/", "+"];
+console.log(evalRPN(arr2));
